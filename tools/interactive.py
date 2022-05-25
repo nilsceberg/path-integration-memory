a471a736a1f8736338dcf39beb41bfbefa9449a4
Nils Ceberg <nils.ceberg@gmail.com>
2022-05-25 15:40:16 +0200

    interactive: better decoding of CPU4 position

--- tools/interactive.py
+++ tools/interactive.py
@@ -5,12 +5,29 @@ from matplotlib import animation
 import pygame
 import math
 import numpy as np
-
+from loguru import logger
 from pygame.locals import *
 from pim.models.stone import bee_simulator, central_complex, cx_basic, cx_rate, trials
+import scipy
+
+def model(args, x, noise = 0.0):
+    return args[0] * np.cos(np.pi + 2*x + args[1])
+
+def error_gradient(data, m, r, theta):
+    x = np.linspace(0, 2*np.pi, data.size, endpoint=False)
+    return (
+        np.sum(2 * (data - m(x))*(-np.cos(np.pi + x + theta))),
+        np.sum(2 * (data - m(x))*(r*np.sin(np.pi + x + theta)))
+    )
+
+def fit(data):
+    xs = np.linspace(0, 2*np.pi, central_complex.N_CPU4, endpoint = False)
+    error = lambda args: model(args, xs) - data
+    params, _ = scipy.optimize.leastsq(error, np.array([0.1, 0.01]))
+    return params
 
 def world_to_screen(pos):
-    pos = np.array([1.0, -1.0]) * 0.1 * pos
+    pos = np.array([1.0, -1.0]) * 0.01 * pos
     return (pos + np.array([1, 1])) * 0.5 * AREA + np.array([AREA, 0])
 
 def graph(display, title, f, origin, size, xaxis, yaxis = (-1.0, 1.0), points = []):
@@ -29,8 +46,8 @@ def graph(display, title, f, origin, size, xaxis, yaxis = (-1.0, 1.0), points = 
     for p in range(0, width):
         u = xaxis[0] + p * xscale
         v = f(u)
-        x = np.int(u / xscale)
-        y = np.int(v / yscale)
+        x = int(u / xscale)
+        y = int(v / yscale)
         display.set_at(origin + (x, -y), (255, 255, 255))
 
     for p in points:
@@ -50,7 +67,7 @@ pygame.init()
 AREA = math.floor(1920 / 2.1)
 WINDOW_SIZE = (2 * AREA, AREA)
 
-MAX_SPEED = 1.0
+MAX_SPEED = 10.0
 MAX_ANGULAR_SPEED = 3.0
 TURN_SHARPNESS = 1.0
 
@@ -78,12 +95,22 @@ font = pygame.font.SysFont("Monospace", 16)
 clock = pygame.time.Clock()
 
 # TB and memory:
-cx = cx_basic.CXBasic()
+#cx = cx_basic.CXBasic()
+cx = cx_rate.CXRate(0.1)
 #cx = cx_rate.CXRatePontin(0.1)
 tb1 = np.zeros(central_complex.N_TB1)
 memory = 0.5 * np.ones(central_complex.N_CPU4)
+#memory = CPU4Memory()
+cpu4 = np.copy(memory)
 motor = 0
+last_decoded = []
 
+def decode_cpu4(cpu4_output):
+    cpu4_normalized = (cpu4 - 0.5)*2.0
+    params = fit(cpu4_normalized)
+    return params
+
+decoded_polar = np.array([0, 0])
 while running:
     dt = clock.get_time() / 1000
     for event in pygame.event.get():
@@ -132,40 +159,58 @@ while running:
     if speed > 0.00001:
         position += velocity
         # Is this where we went wrong? trials.py line 138, 139
-        tl2, cl1, tb1, tn1, tn2, memory, cpu4, cpu1, motor = trials.update_cells(heading, np.flip(velocity), tb1, memory, cx)
+
+        h = heading #(2.0 * np.pi - (heading + np.pi)) % (2.0 * np.pi)
+        v = np.array([np.sin(h), np.cos(h)]) * speed * MAX_SPEED * dt
+        tl2, cl1, tb1, tn1, tn2, memory, cpu4, cpu1, motor = trials.update_cells(h, v, tb1, memory, cx)
+        #motor = -motor
+
+        #decoded_polar = cx.decode_cpu4(cpu4) #decode_position(cpu4_mem.reshape(2, -1), cpu4_mem_gain)
+        #last_decoded = (last_decoded + [np.array([
+        #    np.cos(decoded_polar[0]),
+        #    -np.sin(decoded_polar[0]),
+        #]) * decoded_polar[1] * 0.10])[-1:]
+
+        decoded_polar = decode_cpu4(cpu4) #decode_position(cpu4_mem.reshape(2, -1), cpu4_mem_gain)
+        last_decoded = (last_decoded + [np.array([
+            np.cos(decoded_polar[1] + np.pi),
+            -np.sin(decoded_polar[1] + np.pi),
+        ]) * decoded_polar[0] * 300.0])[-16:]
+
+    decoded = np.mean(np.array(last_decoded), 0)
 
     # background of window
     display.fill((50,50,50))
 
-
     # Draw bee
-    decoded_polar = cx.decode_cpu4(cx.cpu4_output(memory)) #decode_position(cpu4_mem.reshape(2, -1), cpu4_mem_gain)
-    decoded = np.array([
-        np.cos(decoded_polar[0]),
-        np.sin(decoded_polar[0]),
-    ]) * decoded_polar[1] * 1.0
-
     # animate bee
     current_time = pygame.time.get_ticks()
     if current_time - last_update >= 80:
         current_frame = (current_frame + 1) % len(animation_list)
         last_update = current_time
 
+    # home vector
+    pygame.draw.line(surface=display, start_pos=world_to_screen((0, 0)), end_pos=world_to_screen(position), color=(128, 128, 128))    
+
     # rotate bee
     frame_copy = pygame.transform.rotate(animation_list[current_frame], np.degrees(heading) - 90)
+    center_position = tuple(map(lambda i, j: i-j, world_to_screen(decoded), (int(frame_copy.get_width() / 2), int(frame_copy.get_height() / 2))))
+    frame_copy.set_alpha(128)
+    display.blit(frame_copy, center_position)
     center_position = tuple(map(lambda i, j: i-j, world_to_screen(position), (int(frame_copy.get_width() / 2), int(frame_copy.get_height() / 2))))
+    frame_copy.set_alpha(255)
     display.blit(frame_copy, center_position)
 
-    pygame.draw.circle(surface=display, center=world_to_screen(decoded), color=(128, 128, 0), radius=8)
-    pygame.draw.line(surface=display, start_pos=world_to_screen(decoded), end_pos=world_to_screen(decoded + direction * 0.1), color=(128, 128, 128))
+    #pygame.draw.circle(surface=display, center=world_to_screen(decoded), color=(128, 128, 0), radius=8)
+    #pygame.draw.line(surface=display, start_pos=world_to_screen(decoded), end_pos=world_to_screen(decoded + direction * 0.1), color=(128, 128, 128))
 
-    pygame.draw.line(surface=display, start_pos=world_to_screen((0, 0)), end_pos=world_to_screen(position), color=(128, 128, 128))    
+    # GUI divider
     pygame.draw.line(surface=display, start_pos=(AREA, 0), end_pos=(AREA, AREA), color=(128, 128, 128))
 
-    # print(memory)
-    tb1f = lambda x: (1.0 + np.cos(np.pi + x + heading)) / 2.0
-    graph(display, "TB1", tb1f, (20, AREA - 70), (AREA - 40, 100), (0, 2 * np.pi), points=[(x * (2 * np.pi / 8), y) for x, y in enumerate(tb1)])
-    graph(display, "CPU4", lambda x: 0.5, (20, AREA - 200), (AREA - 40, 100), (0, 16), (-0.02, 0.02), points=[(n, y) for n, y in enumerate(memory - 0.5)])
+    #print(memory)
+    tb1f = lambda x: (1.0 + np.cos(np.pi + x - heading)) / 2.0
+    graph(display, "TB1", tb1f, (20, AREA - 150), (AREA - 40, 300), (0, 2 * np.pi), points=[(x * (2 * np.pi / 8), y) for x, y in enumerate(tb1)])
+    graph(display, "CPU4", lambda x: model(decoded_polar, x), (20, AREA - 500), (AREA - 40, 380), (0, 2 * np.pi), (-0.32, 0.32), points=[(n / 16 * 2 * np.pi, y) for n, y in enumerate((cpu4 - 0.5) * 2.0)])
 
     debug_text = font.render(f"Homing: {homing} | True distance from home: {np.linalg.norm(position):.02f} | Perceived distance from home: {np.linalg.norm(decoded):.02f}", False, (255, 255, 255))
     display.blit(debug_text, (8, 8))
