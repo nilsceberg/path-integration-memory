d31932cee0e575be7942fb96d525f762ae71257c
Jacob Säll Nilsson <jacob.e.s.nilsson@gmail.com>
2022-06-14 18:50:49 +0200

    winge: add stone constants as well

--- lib/pim/models/new/winge/constants.py
+++ lib/pim/models/new/winge/constants.py
@@ -11,3 +11,15 @@ cl1_bias_tuned = -0.5
 
 # Preference angles? for CL1
 TL_angles = np.tile(np.arange(0,8),2)*np.pi/4
+
+# Constants
+N_TL2 = 16
+N_CL1 = 16
+N_TB1 = 8
+N_TN1 = 2
+N_TN2 = 2
+N_CPU4 = 16
+N_Pontin = 16
+N_CPU1A = 14
+N_CPU1B = 2
+N_CPU1 = N_CPU1A + N_CPU1B
